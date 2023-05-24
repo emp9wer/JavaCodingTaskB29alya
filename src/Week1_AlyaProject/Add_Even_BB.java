a191cd75a0afaffb266f8807bdd9178f05d30a60
braja <bboukant18@gmail.com>
2023-05-24 19:37:56 -0400

    created Odd Even BB class

---
+++ src/Week1_AlyaProject/Add_Even_BB.java
@@ -0,0 +1,21 @@
+package Week1_AlyaProject;
+
+public class Add_Even_BB {
+
+    public static void main(String[] args) {
+
+        int number= 0;
+
+
+        if(number % 2== 0){
+            System.out.println("number is even = " + number);
+        }else{
+
+            System.out.println("number is odd = " + number);
+        }
+
+
+    }
+
+
+}
