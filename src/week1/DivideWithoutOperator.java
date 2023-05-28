c376b45abc2e647aca78aeccd5e0e90deb12b67d
emp9wer <kirillkandakov@gmail.com>
2023-05-28 15:00:12 -0400

    change

--- src/week1/DivideWithoutOperator.java
+++ src/week1/DivideWithoutOperator.java
@@ -11,16 +11,13 @@ Write a method that can divide two numbers without using division operator.
      */
     public static double divideWoOperator(double number, double divisor) {
 
-
         int res = 0; // we store whole count in this int variable res
         int sign =0; // we store +1 or -1 in this variable depending on condition to return positive or negative
         double decimalCount = 0; // we store decimal count in this double variable decimalCount
 
-
-        if (divisor == 0) { // check if number is 0 - we can not divide by 0
+        if (divisor == 0) { // check if number is 0 - we can not divide by 0.
             throw new ArithmeticException("divisor can not be 0");
         }
-
 
         if (divisor > 0 && number>0) { //if divisor and number is more than 0 ->
             while (number >= divisor) { // we count how many times we can subtract divisor from number
@@ -28,7 +25,6 @@ Write a method that can divide two numbers without using division operator.
                 res++; // write count of iterations in variable res
                 sign=1; // since divisor is positive we use this variable to make result positive
             }
-
 
         } else if(divisor < 0 && number<0){//if divisor and number is less than 0 ->
             divisor = Math.abs(divisor);// we use  Math.abs to get positive value to work with from divisor
@@ -39,7 +35,6 @@ Write a method that can divide two numbers without using division operator.
                 sign=1;// since divisor  and number is negative we use this variable to make result positive
             }
 
-
         }else if(divisor < 0 || number<0) //if divisor or number is less than 0 ->
                 divisor = Math.abs(divisor);// we use  Math.abs to get positive value to work with from divisor
                  number = Math.abs(number);
@@ -49,7 +44,6 @@ Write a method that can divide two numbers without using division operator.
                     sign=-1;// since divisor  or number is negative we use this variable to make result negative
                 }
 
-
         if ( number!=0 ) { //if remainder != 0
             number = number * 100; //multiply remainder by 100
             while (number>=divisor){ //iterate while number >= divisor
@@ -57,7 +51,6 @@ Write a method that can divide two numbers without using division operator.
                 decimalCount+=0.01; //and add to double variable decimalCount 0.01
             }
         }
-
 
         return (res+decimalCount) * sign; // we return result (we add whole count + decimal remainder and make it negative or positive.
     }
