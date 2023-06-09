8e6164ba8922b7170caf47c31eb67d5cf8fb445b
rashajawad <rasha.jawad2015@gmail.com>
2023-06-09 18:42:10 -0400

    my code

---
+++ src/week03/task1/Rasha1.java
@@ -0,0 +1,39 @@
+package week03.task1;
+
+public class Rasha1 {
+    public static boolean isPrime(int number){
+
+
+        for (int i = 2; i < number ; i++) {
+
+
+
+            if(number % i == 0){
+                System.out.println(number + " is not a prime number");
+                return false;
+            }
+
+
+        }
+
+
+        System.out.println(number + " is prime number");
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+
+        isPrime(23);
+
+
+
+    }
+}
+/*
+Numbers--Prime Number
+write a method that can check if a number is prime or not
+ */
+
+
---
+++ src/week03/task2/Rasha2.java
@@ -0,0 +1,22 @@
+package week03.task2;
+
+public class Rasha2 {
+    /*
+Numbers--Reverse negative Number
+write a return method that can reverse negative number and return it as int
+ */
+
+    public class Rasha {
+        public static int reverseNum(int num) {
+            String num1 = Integer.toString(num);
+            String result = "";
+            for (int i = num1.length() - 1; i >= 1; i--) {
+                result += num1.charAt(i);
+            }
+            int numResult = Integer.parseInt(result);
+            return (numResult * (-1));
+
+        }
+    }
+
+}
