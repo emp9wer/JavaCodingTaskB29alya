697bcec04b1b44f1026de8516cfc9f7a0feb8b8b
Fatima.Cydeo <essaborfatimazahraa@gmail.com>
2023-06-25 22:32:07 -0400

    add tasks for week6

--- src/week05/task2/Fatima.java
+++ src/week05/task2/Fatima.java
@@ -11,6 +11,7 @@ public class Fatima {
         String chars ="ABCD";
         String reverseChars="";
 
+
         for (int i = chars.length() - 1; i >= 0; i--) {
             reverseChars += chars.charAt(i);
         }
@@ -21,6 +22,16 @@ public class Fatima {
 
     }
 
+
+    /*
+    In the first iteration, i is assigned the value of chars.length() - 1, which is 3 (since the length of "ABCD" is 4). The character at index 3 in chars is 'D'. Thus, reverseChars becomes "D".
+
+In the second iteration, i is decremented to 2. The character at index 2 in chars is 'C'. reverseChars now becomes "DC".
+
+In the third iteration, i is decremented to 1. The character at index 1 in chars is 'B'. reverseChars is now "DCB".
+
+In the fourth and final iteration, i is decremented to 0. The character at index 0 in chars is 'A'. reverseChars becomes "DCBA".
+     */
 
 /*
 Write a function that can reverse a String
---
+++ src/week06/task1/Fatima.java
@@ -0,0 +1,83 @@
+package week06.task1;
+
+public class Fatima {
+    public static boolean passwordTask () {
+
+        String password = "Fatima1@";
+
+        boolean hasSpecialChar = false;
+
+
+        // 1. Password MUST be at least have 6 characters and should not contain space
+
+        if (password.length() < 6 || password.contains(" ")) {
+            return false;
+        }
+        char letter = 'A';
+
+        //2- PassWord should at least contain one upper-case letter
+
+        for (int i = 'a'; i < 'z'; i++) {
+            if (letter >= 'a' && letter <= 'z') {
+                return true;
+            }
+        }
+
+        //3-PassWord should at least contain one lowercase letter
+
+
+        for (int i = 'A'; i < 'Z'; i++) {
+            if (letter >= 'A' && letter <= 'Z') {
+                return true;
+            }
+        }
+
+        //4. Password should at least contain one special character
+
+        String specChar = "!@#$%^&*()-_=+[]{}\\|;:'\",.<>/?";
+
+        for (char each : password.toCharArray()) {
+            if (specChar.contains(Character.toString(each))) {
+                return true;
+            }
+
+
+            boolean hasDigit = false;
+
+            //5. Password should at least contain a digit
+            for (int i = 0; i < 9; i++) {
+                if (Character.isDigit(password.charAt(i))) {
+
+                    return true;
+                }
+
+
+
+            }
+
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        if (passwordTask()== true ){
+            System.out.println( "The password is correct");
+        }else {
+            System.err.println("The password is incorrect") ;
+        }
+
+
+
+}}
+
+/*
+1. Write a return method that can verify if a password is valid or not.
+-Requirements:
+1. Password MUST be at least have 6 characters and should not contain space
+2. PassWord should at least contain one upper-case letter
+3. PassWord should at least contain one lowercase letter
+4. Password should at least contain one special character
+5. Password should at least contain a digit
+-if all requirements above are met, the method returns true, otherwise returns  false
+ */
