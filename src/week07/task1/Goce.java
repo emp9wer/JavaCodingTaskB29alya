c47a9bca1654a3760325014a9454d5fd59a20a23
Goce_Cydeo <goce.trpevski@gmail.com>
2023-07-03 18:22:19 -0400

    Comments are added in 'minimum number from an int Array' task.

--- src/week07/task1/Goce.java
+++ src/week07/task1/Goce.java
@@ -13,10 +13,10 @@ public class Goce {
      * @return
      */
     public static int minNum() {
-        int min = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE; // Variable 'min' is a temporary minimum number variable where every number in the array will be compared against this. Temporarily the maximum number value in Java is assigned to it.
         for (int i = 0; i < in.length; i++) {
-            if (in[i] < min) {
-                min = in[i];
+            if (in[i] < min) {// Always true because every number in the array will be smaller than the maximum number value that is in Java. Variable 'min' wil be reassigned with every array number in turn and compared with the next number in the same array.
+                min = in[i];//Every number in the array will be assigned in the 'min' variable. Will end up with the minimum value.
             }
         }
         return min;
@@ -29,10 +29,10 @@ public class Goce {
 
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the size of the array: ");
-        in = new int[input.nextInt()];
+        in = new int[input.nextInt()];// defining the size of an array by inputting it with Scanner.
         System.out.println("Please enter the numbers to be added in the array: ");
         for (int i = 0; i < in.length; i++) {
-            in[i] = input.nextInt();
+            in[i] = input.nextInt();// Entering the numbers in the array through loop
         }
 
         System.out.println("Minimum number of the array you've entered is: " + minNum());
