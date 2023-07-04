deb4861073994369624f5f17f7d95bf3e355b6da
Goce_Cydeo <goce.trpevski@gmail.com>
2023-07-04 11:09:22 -0400

    Sorting array elements in accenting order task updated with approach where method accepts parameter.

--- src/week07/task2/Goce.java
+++ src/week07/task2/Goce.java
@@ -11,35 +11,64 @@ public class Goce {
 
      */
 
+
+    //Scanner input approach:
     /**
      * Method returns elements from an array in ascending order
      * @return
      */
-    public static List<Integer> ascendingArr() {
+//    public static List<Integer> ascendingArr() {
+//
+//        List<Integer> arrList = new ArrayList<>();
+//
+//        for (int i = 0; i <arrNums.length; i++) {// Iterates through the array and adds array elements into the arrayList.
+//            arrList.add(arrNums[i]);
+//        }
+//        Collections.sort(arrList);// sort() from Collections class sorts elements in accenting order
+//        return arrList;// Returns the list with elements in ascending order.
+//    }
+//
+//    static int[] arrNums;
 
+
+    //Parameter input aproach:
+    static int[] arr = {10, 9, 8, 7};
+
+    /**
+     * Method accepts an array parameter and returns elements from that array in ascending order
+     * @param input
+     * @return
+     */
+    public static List<Integer> ascendingArr(int[] input) {
         List<Integer> arrList = new ArrayList<>();
-
-        for (int i = 0; i <arrNums.length; i++) {// Iterates through the array and adds array elements into the arrayList.
-            arrList.add(arrNums[i]);
+        for (int i = 0; i < arr.length; i++) {// Iterates through the array and adds array elements into the arrayList.
+            arrList.add(arr[i]);
         }
         Collections.sort(arrList);// sort() from Collections class sorts elements in accenting order
         return arrList;// Returns the list with elements in ascending order.
     }
 
-    static int[] arrNums;
+
+
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        System.out.println("Please enter the size of the array: ");
-        arrNums = new int[input.nextInt()];
-        System.out.println("Please enter the numbers in the array: ");
-        for (int i = 0; i < arrNums.length; i++) {
-            arrNums[i] += input.nextInt();
-        }
+        //Scanner input approach:
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("Please enter the size of the array: ");
+//        arrNums = new int[input.nextInt()];
+//        System.out.println("Please enter the numbers in the array: ");
+//        for (int i = 0; i < arrNums.length; i++) {
+//            arrNums[i] += input.nextInt();
+//        }
+//
+//        System.out.println("Numbers from the array sorted in ascending order are: " + ascendingArr());
 
-        System.out.println("Numbers from the array sorted in ascending order are: " + ascendingArr());
+        //Parameter input approach:
 
+        //Parameter input approach:
+          System.out.println("Numbers from the array sorted in ascending order are: " + ascendingArr(arr));
     }
 
 }
